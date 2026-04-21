95393f4fb5739d3020e1528eec4e3a6fac836453
arunkumar0108 <kmarunkumarar0108@gmail.com>
2026-04-21 12:42:52 +0530

    Components added

--- frontend/irbs-frontend/src/app/app.routes.ts
+++ frontend/irbs-frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { TrainListComponent } from './pages/train-list/train-list.component';
+import { SeatSelectionComponent } from './pages/seat-selection/seat-selection.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LoginComponent},
+    { path: 'home', component: HomeComponent},
+    { path: 'trains', component: TrainListComponent},
+    { path: 'seats', component: SeatSelectionComponent}
+];
+
---
+++ frontend/irbs-frontend/src/app/pages/train-list/train-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-train-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './train-list.component.html',
+  styleUrl: './train-list.component.css'
+})
+export class TrainListComponent {
+
+}
