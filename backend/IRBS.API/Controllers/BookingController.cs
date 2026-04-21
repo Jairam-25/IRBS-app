6c607fb190c875b44cec298fcf6faa0511fe77ea
arunkumar0108 <kmarunkumarar0108@gmail.com>
2026-04-21 21:18:44 +0530

    Return statement updated

--- backend/IRBS.API/Controllers/BookingController.cs
+++ backend/IRBS.API/Controllers/BookingController.cs
@@ -40,12 +40,20 @@ namespace IRBS.API.Controllers
                 b.SeatNumber == booking.SeatNumber);
 
             if (exists)
-                return BadRequest("Seat already booked");
+                return BadRequest(new
+                {
+                    success = true,
+                    message = "Seat already booked"
+                });
 
             _context.Bookings.Add(booking);
             await _context.SaveChangesAsync();
 
-            return Ok("Seat booked successfully");
+            return Ok(new
+            {
+                success = true,
+                message = "Seat booked successfully"
+            });
         }
     }
 }
